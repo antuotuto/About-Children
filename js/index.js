0f4a2b5f6415e5d8d8340b28a60183d67d6e1cb3
anan <675988829@qq.com>
2017-08-25 23:50:40 +0800

    1.0.0

---
+++ js/index.js
@@ -0,0 +1,95 @@
+$(document).ready(function() {
+
+
+    $('#fond').on('click', function (data) {
+        var name=[];
+        var nameA = document.getElementById('exampleInputEmail1');
+        var nameB = nameA.value;
+
+
+        if(nameB === '孙琼瑶'){
+
+            window.location.href='./html/sqy.html';
+
+            // window.location.href='./html/'+ name +'.html';
+        }else if(nameB === '王安'){
+            window.location.href='./html/wa.html';
+        }else if(nameB === '黄岳龙'){
+            window.location.href='./html/hyl.html';
+        }else if(nameB === '刘佳阳'){
+            window.location.href='./html/ljy.html';
+        }else if(nameB === '周喆'){
+            window.location.href='./html/zz.html';
+        }else if(nameB === '卢宇'){
+            window.location.href='./html/ly.html';
+        }else if(nameB === '廖旭凯'){
+            window.location.href='./html/lxk.html';
+        }else if(nameB === '杨潇'){
+            window.location.href='./html/yx.html';
+        }else if(nameB === '欧阳晓倩'){
+            window.location.href='./html/oyxq.html';
+        }else if(nameB === '王徐池'){
+            window.location.href='./html/wxc.html';
+        }else if(nameB === '胡鹏'){
+            window.location.href='./html/hp.html';
+        }else if(nameB === '蔡书宇'){
+            window.location.href='./html/csy.html';
+        }else if(nameB === '随越琦'){
+            window.location.href='./html/syq.html';
+        }else if(nameB === '柳思宁'){
+            window.location.href='./html/lsl.html';
+        }else if(nameB === '李晨红'){
+            window.location.href='./html/lch.html';
+        }else if(nameB === '狗逼'){
+            window.location.href='./html/hp.html';
+        }else if(nameB === '狗子'){
+            window.location.href='./html/hp.html';
+        }else if(nameB === '狗妹'){
+            window.location.href='./html/lsl.html';
+        }else if(nameB === '蔡妹仔'){
+            window.location.href='./html/csy.html';
+        }else if(nameB === '喆妹'){
+            window.location.href='./html/lch.html';
+        }else if(nameB === '安妹'){
+            window.location.href='./html/sqy.html';
+        }else if(nameB === '小白'){
+            window.location.href='./html/hyl.html';
+        }else if(nameB === '小朋友'){
+            window.location.href='./html/wxc.html';
+        }else if(nameB === '蔡妹'){
+            window.location.href='./html/oyxq.html';
+        }else if(nameB === '晓倩阿姨'){
+            window.location.href='./html/oyxq.html';
+        }else if(nameB === '傻逼安'){
+            window.location.href='./html/wa.html';
+        }else if(nameB === '极客安'){
+            window.location.href='./html/wa.html';
+        }else if(nameB === '睡神'){
+            window.location.href='./html/ljy.html';
+        }else if(nameB === '人兽'){
+            window.location.href='./html/zz.html';
+        }else if(nameB === '话费仔'){
+            window.location.href='./html/ly.html';
+        }else if(nameB === '凯逼'){
+            window.location.href='./html/lxk.html';
+        }else{
+            window.location.href='./html/新年快乐.html';
+        }
+
+
+
+
+
+
+
+
+
+
+
+    });
+
+
+
+
+
+});
